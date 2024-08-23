7f066ece82ba2a934c4363a5f3440b5b964d63bb
angjun04 <peteryoung04@naver.com>
2024-08-23 18:03:57 +0900

    supabase db 연동 코드 / 추후 프로덕션 시 env 사용 필요

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
-import 'package:eomeonada/Home.dart';
+//import 'package:eomeonada/Home.dart';
 import 'package:flutter/material.dart';
 import 'Login.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://lbgqvqsioqayyqrapeae.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZ3F2cXNpb3FheXlxcmFwZWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2ODE5MzIsImV4cCI6MjAzOTI1NzkzMn0.EgHPmLy1XtX4BEFhfwK2Fz3-BnsqyuU8ZRjxA34QYaQ',
+  ); // supabase db 연동
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
